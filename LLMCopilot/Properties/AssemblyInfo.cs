8cb61e3424ecdd38b0595178a297dd7059170c8b
Dan Booth <dan@diplo.co.uk>
2026-03-29 17:47:11 +0100

    Remove migration

--- LLMCopilot/Properties/AssemblyInfo.cs
+++ LLMCopilot/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OllamaPilot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LLM Pilot using Ollama")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Diplodocus Squad")]
 [assembly: AssemblyProduct("OllamaPilot")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
